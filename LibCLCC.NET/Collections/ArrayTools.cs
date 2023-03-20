68add8d954b31b65dfcdcaec044af34547fc0b64
Creeper Lv <creeperlv@users.noreply.github.com>
2023-03-21 00:25:19 +0800

    Working on a general purpose parser. The parser is currently not working.

---
+++ LibCLCC.NET/Collections/ArrayTools.cs
@@ -0,0 +1,20 @@
+﻿namespace LibCLCC.NET.Collections {
+    /// <summary>
+    /// Tools for arrays.
+    /// </summary>
+    public static class ArrayTools {
+        /// <summary>
+        /// Is an array contains an element.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static bool Contains<T>(T[] arr,T t) {
+            foreach (var item in arr) {
+                if(item.Equals(t)) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ LibCLCC.NET/TextProcessing/Parser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LibCLCC.NET.TextProcessing {
+    /// <summary>
+    /// General purpose parser. If you are building a compiler against it, you may need a second-pass parser to convert operators.
+    /// </summary>
+    public class Parser {
+        /// <summary>
+        /// Works for operators. e.g.: ==, --
+        /// </summary>
+        public char[] Splitters = new char[] { ' ', '\t', '\r', '\n' };
+        public List<string> PredefinedSegmentTemplate = new List<string>();
+        public List<char> SegmentEncapsulationIdentifiers = new List<char>();
+        public List<LineCommentIdentifier> lineCommentIdentifiers = new List<LineCommentIdentifier>();
+        public List<ClosableCommentIdentifier> closableCommentIdentifiers = new List<ClosableCommentIdentifier>();
+        /// <summary>
+        /// Parse segment
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="DisableCommentChecker"></param>
+        /// <returns></returns>
+        public Segment Parse(string str, bool DisableCommentChecker) {
+            Segment root = new Segment();
+            Segment current = root;
+            bool isSegmentEncapsulation = false;
+            string attention = "";
+            using (StreamReader sr = new StreamReader(str)) {
+                while (true) {
+                    int b8 = sr.Read();
+                    if (b8 == -1) break;
+                    char c = (char)b8;
+                    current.content += b8;
+                    if (isSegmentEncapsulation) {
+
+                    }
+                    else {
+                        if (Splitters.Contains<char>(c)) {
+                            CloseSegment();
+                        }
+                        else {
+                            foreach (var item in PredefinedSegmentTemplate) {
+                                if (item[attention.Length] == c) {
+                                    attention += c;
+                                }
+                                if (item == attention) {
+                                    /**
+                                     * Separate segment
+                                     * **/
+                                    CloseSegment();
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            void CloseSegment() {
+
+            }
+            return root;
+        }
+    }
+
+    /// <summary>
+    /// One line comment like // or #
+    /// </summary>
+    public class LineCommentIdentifier {
+        public char[] StartSequence;
+    }
+    /// <summary>
+    /// Closable comment like /* */
+    /// </summary>
+    public class ClosableCommentIdentifier {
+        public string Start;
+        public string End;
+    }
+    /// <summary>
+    /// One segment
+    /// </summary>
+    public class Segment {
+        public Segment Prev = null;
+        public Segment Next = null;
+        public string content;
+        public bool isEncapsulated;
+        public char EncapsulationCharacter;
+    }
+}
