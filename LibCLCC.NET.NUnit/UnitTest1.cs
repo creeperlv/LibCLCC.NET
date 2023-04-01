40f427f8295f5031ecc80fe6a57344702d2c34bc
Creeper Lv <creeperlv@users.noreply.github.com>
2023-04-02 04:50:25 +0800

    Fixed a problem in the NUnit that stops automatical build.

--- LibCLCC.NET.NUnit/UnitTest1.cs
+++ LibCLCC.NET.NUnit/UnitTest1.cs
@@ -1,28 +1,32 @@
 using LibCLCC.NET.TextProcessing;
 using System.Diagnostics;
 
-namespace LibCLCC.NET.NUnit {
-    public class Tests {
+namespace LibCLCC.NET.NUnit
+{
+    public class Tests
+    {
         string _content;
         string __content;
         GeneralPurposeParser parser;
         CommandLineParser CLP;
         [SetUp]
-        public void Setup() {
+        public void Setup()
+        {
             _content = "namespace LibCLCC.NET.NUnit {\r\n    public class Tests {\r\n\tstring _content;\r\n        [SetUp]\r\n        public void Setup() {\r\n            _content = \"a\";\r\n        }\r\n\r\n        [Test]\r\n        public void Test1() {\r\n            Assert.Pass();\r\n        }\r\n    }\r\n}";
             __content = "rm /* -rf --no-preserve-root";
-            parser=new GeneralPurposeParser();
-            CLP=new CommandLineParser();
+            parser = new GeneralPurposeParser();
+            CLP = new CommandLineParser();
         }
 
         [Test]
-        public void Test1() {
-            var s=parser.Parse(_content,false,"117");
-            Console.WriteLine(s.SequentialToString("->",true,true));
-            var _s=CLP.Parse(__content,false);
-            Console.WriteLine(_s.SequentialToString(" "),true);
-            GeneralPurposeParser.Concatenate(s, _s);
-            Console.WriteLine(s.SequentialToString("->",true,true));
+        public void Test1()
+        {
+            var s = parser.Parse(_content, false, "117");
+            Console.WriteLine(s.SequentialToString("->", true, true));
+            var _s = CLP.Parse(__content, false);
+            Console.WriteLine(_s.SequentialToString(" "), true);
+            s.Concatenate(_s);
+            Console.WriteLine(s.SequentialToString("->", true, true));
             Assert.Pass();
         }
     }
