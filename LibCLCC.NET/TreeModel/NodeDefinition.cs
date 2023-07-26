83f30283f647836fafdea7f72a28c7d86ca8c89f
Creeper Lv <creeperlv@users.noreply.github.com>
2023-07-27 02:45:37 +0800

    Added a few interfaces to provide node definition.

---
+++ LibCLCC.NET/TreeModel/NodeDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LibCLCC.NET.TreeModel
+{
+	/// <summary>
+	/// Basic definition of a node.
+	/// </summary>
+	public class NodeDefinition
+	{
+		/// <summary>
+		/// The name of the node.
+		/// </summary>
+		public string Name;
+		/// <summary>
+		/// Max count of children. <0 for infinite children, 0 for no child, and any positive number is a value fot
+		/// </summary>
+		public int MaxChildrenCount;
+		/// <summary>
+		/// Properties of the node.
+		/// </summary>
+		public List<string> Properties;
+	}
+}
